1cf062698b9f82602881f6f7bb6dfda0cf6017dd
Daniel Kaplan <daniel.scott.kaplan@gmail.com>
2012-07-02 15:33:26 -0500

    added problem descriptions

--- #1.rb
+++ #1.rb
@@ -1,7 +1,17 @@
-  @a = []
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+# 
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
+@a = []
 1000.times do |number|
   if number % 3 == 0 || number % 5 == 0
     @a.push(number) 
     end
 end
-   puts @a
+
+sum = 0
+@a.each do |i|
+  sum += i
+end
+puts sum
+
--- #5.rb
+++ #5.rb
@@ -1,7 +1,13 @@
+# 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+# 
+# What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
 @a = []
-for j jn 1..1000000000
-  jf  j % 2 == 0 && j%3==0 && j%4==0 && j%5==0 && j%6==0 && j%7==0 && j%8==0 && j%9==0 && j%10==0 && j%11==0 && j%12==0 && j%13==0 && j%14==0 && j%15==0 && j%16==0 && j%17==0 && j%18==0 && j%19==0 && j%20==0            
+for j in 1..1000000000
+  if  j%2== 0 && j%3==0 && j%4==0 && j%5==0 && j%6==0 && j%7==0 && j%8==0 && j%9==0 && j%10==0 && j%11==0 && j%12==0 && j%13==0 && j%14==0 && j%15==0 && j%16==0 && j%17==0 && j%18==0 && j%19==0 && j%20==0            
     @a.push(j)
   end
 end
 puts @a
+
+#first number you see is answer

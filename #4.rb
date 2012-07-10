de78e4aa5d0f571d9de40d4b4c8dd5f1e1c7e290
Daniel Kaplan <daniel.scott.kaplan@gmail.com>
2012-07-10 16:38:29 -0500

    #4 now perfect

--- #4.rb
+++ #4.rb
@@ -1,11 +1,10 @@
 # A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 99.
 # 
 # Find the largest palindrome made from the product of two 3-digit numbers.
-a = []
-b = []
+B = []
 def check_if_palindrome(number)
   if number.to_s == number.to_s.reverse
-    puts "#{number} "
+    B << number
   end
 end
 
@@ -16,6 +15,8 @@ for x in 100..999
   end
 end
 
-# returns all palindromes, search through output for highest
-# Answer: 906609 
+puts B.max
+# takes 2 seconds
+
+
 

19579922c84ac5074ec44c94f0118a8377e0f5b4
dkaplan88 <dkaplan@g2labs.net>
2013-06-02 23:30:55 -0500

    number 20 solved

---
+++ #20.rb
@@ -0,0 +1,15 @@
+# n! means n  (n  1)  ...  3  2  1
+
+# For example, 10! = 10  9  ...  3  2  1 = 3628800,
+# and the sum of the digits in the number 10! is 
+# 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+
+# Find the sum of the digits in the number 100!
+array = []
+answer = 0
+
+100.downto(1) { |i| array <<  i } 
+array.inject(&:*).to_s.each_char { |char| sum += char.to_i }
+
+# answer => 648
+# time =>  0.118 milliseconds
